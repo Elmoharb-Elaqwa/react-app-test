73c87a8260cc851162b28a54323499ae59ca436b
Elmoharb-Elaqwa <abdelrhmansholkamy1999@gmail.com>
2022-03-06 07:27:08 +0200

    first deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { notesRouter } = require('./api/v1/indexNote');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORST || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
